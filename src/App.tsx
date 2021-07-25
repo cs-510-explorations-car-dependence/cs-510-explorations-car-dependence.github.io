56121df846a4162b173e095e31098e7b37da1a92
Bradon Kanyid <rattboi@gmail.com>
2021-07-24 23:04:09 -0700

    Get test routes from backend app instead of hardcoded

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import Map from "./components/Map";
 import "./App.css";
 import { Polyline, Tooltip } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 
-const TEST_POLYLINE = [
-  [45.5, -122.7],
-  [45.52, -122.72],
-  [45.49, -122.68],
-] as LatLngExpression[];
-
 const MAP_FILL_SCREEN_STYLE = { width: "100vw", height: "100vh" };
 
-function App() {
-  return (
-    <div className="App">
-      <Map style={MAP_FILL_SCREEN_STYLE}>
-        <Polyline positions={TEST_POLYLINE}>
-          <Tooltip direction="bottom" offset={[0, 20]} permanent>
-            Test tooltip
-          </Tooltip>
-        </Polyline>
-      </Map>
-    </div>
-  );
+class App extends Component {
+  state = {
+    error: false,
+    isLoaded:  false,
+    routes: []
+  };
+
+  componentDidMount() {
+    //fetch("http://127.0.0.1:5000/api/route/", {})
+    fetch("https://car-dependence-backend.herokuapp.com/api/route/", {})
+      .then(res => res.json())
+      .then(
+        (res) => {
+          this.setState({
+            isLoaded: true,
+            routes: res.route
+          });
+          console.log(res);
+        },
+        (err) => {
+          this.setState({
+            isLoaded: true,
+            error: true
+          });
+          console.log(err);
+      }
+    )
+  }
+
+  render() {
+    const { error, isLoaded, routes } = this.state;
+    if (error) {
+      return <div>Error</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <Map style={MAP_FILL_SCREEN_STYLE}>
+            <Polyline positions={routes}>
+              <Tooltip direction="bottom" offset={[0, 20]} permanent>
+                Test tooltip
+              </Tooltip>
+            </Polyline>
+          </Map>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
