39c53d3161725179f46108415685d903084495b1
Matt O'Tousa <mlotousa@gmail.com>
2021-07-29 10:29:46 -0700

    working on getting bbox on pan/zoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Map from "./components/Map";
 import "./App.css";
+import { Map as LeafletMapData } from "leaflet";
 import { Polyline, Tooltip } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 import {
@@ -20,16 +21,24 @@ const API_URL = "https://car-dependence-backend.herokuapp.com/api/route/";
 
 const MAP_FILL_SCREEN_STYLE = { width: "100vw", height: "100vh" };
 
+// Portland
+const INITIAL_LAT_LON = [45.5051, -122.675] as LatLngExpression;
+const INITIAL_ZOOM = 13;
+
+const getBBox = ({ map }: { map: }) => {
+
+}
+
 function App() {
-  // useFetch needs the application/json header to parse response properly
-  // Server doesn't actually care
+  const [mapState, setMapState] = React.useState(null as LeafletMapData?);
   const apiState: AsyncState<ServerResponse> = useFetch(API_URL, {
     headers: { accept: "application/json" },
   });
+  apiState.run({});
 
   return (
     <div className="App">
-      <Map style={MAP_FILL_SCREEN_STYLE}>
+      <Map style={MAP_FILL_SCREEN_STYLE} center={INITIAL_LAT_LON} zoom={13} whenCreated={setMapState}>
         <IfPending state={apiState}>Loading...</IfPending>
         <IfRejected state={apiState}>Error Message</IfRejected>
         <IfFulfilled state={apiState}>
