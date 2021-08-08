196b55f500be0d7ba070283128d59e98e296eeb2
Matt O'Tousa <mlotousa@gmail.com>
2021-08-08 13:48:50 -0700

    fixed map sizing

--- src/components/Map/MapCom.tsx
+++ src/components/Map/MapCom.tsx
@@ -2,12 +2,11 @@ import "./../../App.css";
 import React, { useState } from "react";
 import "../../css/landingPage.css";
 import Map from "./Map";
-import { Polyline, Tooltip } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 import { Map as LeafletMapData } from "leaflet";
 import MapDataLayer from "../MapDataLayer";
 
-const MAP_FILL_SCREEN_STYLE = { width: "100vw", height: "100vh" };
+const MAP_FILL_SCREEN_STYLE = { width: "100vw", height: "calc(100vh - 70px)" };
 const INITIAL_LAT_LON = [45.5051, -122.675] as LatLngExpression;
 const INITIAL_ZOOM = 13;
 
@@ -36,32 +35,5 @@ function MapCom() {
     </Map>
   );
 }
-
-/*
-export default class MapCom extends Component{
-
-
-// just a simple navigation component
-
-
-    render(){
-
-        
-
-        
-        return (
-            <Map style={MAP_FILL_SCREEN_STYLE}>
-        <Polyline positions={TEST_POLYLINE}>
-          <Tooltip direction="bottom" offset={[0, 20]} permanent>
-            Test tooltip
-          </Tooltip>
-        </Polyline>
-      </Map>
-            
-        );
-
-    }
-}
-*/
 
 export default MapCom;
