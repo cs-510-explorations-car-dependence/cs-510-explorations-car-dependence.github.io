f1327d2a45c2d5902f6c16426c00e2a76ba549ea
Matt O'Tousa <mlotousa@gmail.com>
2021-08-08 14:35:16 -0700

    fix unused warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "./App.css";
 import Face from "../src/components/Face/Face";
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,31 +1,18 @@
 import "./../../App.css";
-import React, {Component} from "react";
+import React from "react";
 import "../../css/landingPage.css";
-import { Link } from "react-router-dom";
 
+export default class Nav extends React.Component {
+  /*just a simple navigation component, */
 
-
-export default class Nav extends React.Component{
-
-
-/*just a simple navigation component, */
-
-    
-    render(){
-
-    
-        return (
-            <nav>
-                <ul className="navs">
-                    <li onClick={() => this.props.action(1)}> 
-                        About ismyroadfresh
-                        
-                    </li>
-                    <li onClick={() => this.props.action(55)}>
-                        Get Started
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav>
+        <ul className="navs">
+          <li onClick={() => this.props.action(1)}>About ismyroadfresh</li>
+          <li onClick={() => this.props.action(55)}>Get Started</li>
+        </ul>
+      </nav>
+    );
+  }
 }
