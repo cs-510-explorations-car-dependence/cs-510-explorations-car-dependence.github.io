e1ac0232556bc9a0620518a6d49d979fd6ceab7b
Matt O'Tousa <mlotousa@gmail.com>
2021-08-09 21:31:55 -0700

    Valid latlon only (#35)
    * Fixed invalid longitude bug
    
    If the center of the map is found to be an invalid lon (outside of -180
    to 180) find the real equivalent longitude and snap map to that
    
    * cleanup
    
    * comments!

--- src/hooks/useMapBBox.ts
+++ src/hooks/useMapBBox.ts
@@ -1,12 +1,23 @@
 import { useState, useCallback, useEffect } from "react";
-import { Map } from "leaflet";
+import { LatLng, Map } from "leaflet";
+
+const asValidLon = (lon: number) => (((lon % 360) + 540) % 360) - 180;
 
 // gets a bounding box and dynamically updates it based on the currently visible map
 function useBBox(map: Map) {
   const [bbox, setBBox] = useState(map.getBounds());
 
   const onMove = useCallback(() => {
-    setBBox(map.getBounds());
+    const center = map.getCenter();
+    // longitude can cause problem, since leaflet scrolls forever left/right and longitude grows/shrinks accordingly
+    if (center.lng >= -180 && center.lng <= 180) {
+      setBBox(map.getBounds());
+      // if its out of range, snap back to real-world coordinate
+    } else {
+      const validLon = asValidLon(center.lng);
+      const newCenter = new LatLng(center.lat, validLon);
+      map.setView(newCenter, undefined, { animate: false });
+    }
   }, [map]);
 
   // adds the required event listeners for Leaflet to handle this properly
